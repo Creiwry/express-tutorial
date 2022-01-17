b0cd91546cf98e521fe22945ff126fd940dece24
Jay Gardos <jay.gardos@outlook.com>
2022-01-17 09:35:02 +0100

    middlewear first

--- app.js
+++ app.js
@@ -1,65 +1,30 @@
 const express = require('express');
-
+const logger = require('./logger')
+const authorize = require('./authorize')
 const app = express();
 
-const {products} = require('./data')
+// req => middleware => res
 
-app.get('/', (req, res)=>{
-    res.send('<h1> Home Page</h1> <a href="/api/products">products</a>');
+app.use([logger, authorize]);
 
+
+app.get('/', (req, res) =>{
+    res.send('Home');
+})
+
+app.get('/about', (req, res)=>{
+    res.send('About');
 })
 
 app.get('/api/products', (req, res)=>{
-    const newProducts = products.map((product)=>{
-        const {id, name, image} = product;
-        return {id, name, image};
-    })
-
-    res.json(newProducts)
-})
-
-app.get('/api/products/:productID', (req, res)=>{
-/*     console.log(req);
-    console.log(req.params); */
-
-    const {productID} = req.params;
-    const singleProduct = products.find((product)=> product.id === Number(productID))
-    
-    if(!singleProduct){
-        return res.status(404).send('product does not exist')
-    }
-    res.json(singleProduct)    
-})
-
-app.get('/api/v1/query', (req, res)=>{
-//    console.log(req.query);
-const {search, limit} = req.query;
-    let sortedProducts = [...products];
-
-    if(search){
-        sortedProducts = sortedProducts.filter((product)=> {
-            return product.name.startsWith(search)
-        })
-    } 
-
-    if(limit){
-        sortedProducts = sortedProducts.slice(0, Number(limit));
-    }
-
-    if(sortedProducts.length < 1){
-        return res.status(200).json({success:true, data:[]});
-    }
-
-    return res.status(200).json(sortedProducts);
-})
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res)=>{
-    console.log(req.params);
-    res.send('hello world');
+    res.send('Products');
 })
 
 
-
+app.get('/api/items', (req, res)=>{
+    console.log(req.user);
+    res.send('Items');
+})
 
 
 app.listen(5000, ()=>{
